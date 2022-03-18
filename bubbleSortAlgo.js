6e1f5cc38bf4725aa9766655c8c753f355a63f8e
Adekunle Ashaolu <dekxnle@gmail.com>
2022-03-18 14:56:35 +0100

    bubble sort algo

---
+++ bubbleSortAlgo.js
@@ -0,0 +1,32 @@
+function bubbleSort(originalArray) {
+  let swapped = false;
+
+  const array = [...originalArray];
+
+  for (let i = 0; i < array.length; i++) {
+    // set swapped back to false on every iteration
+    swapped = false;
+
+    console.log(array);
+
+    // this loop is responsible for swapping the positions of array elements
+    for (let j = 0; j < array.length; j++) {
+      // compare array elements
+      if (array[j + 1] < array[j]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      return array;
+    }
+  }
+
+  return array;
+}
+
+const names = [ada, emma, tunde, jet - li, wale];
+
+console.log(bubbleSort(names));
