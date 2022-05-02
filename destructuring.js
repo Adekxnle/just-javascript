6e4fe19039761cffe554a1d0b84a5862d07a068b
Adekunle Ashaolu <dekxnle@gmail.com>
2022-05-02 03:48:21 +0100

    no more examples, for now.

--- destructuring.js
+++ destructuring.js
@@ -1,49 +1,49 @@
-// decontructing complex arrays
-// const people = [
-//   {
-//     id: 1,
-//     name: {
-//       alias: 'kunle',
-//       lastname: 'adekunle',
-//       surname: 'ashaolu',
-//     },
-//     age: 20,
-//   },
-//   {
-//     id: 2,
-//     name: {
-//       alias: 'jay',
-//       lastname: 'jayson',
-//       surname: 'tatum',
-//     },
-//     age: 26,
-//   },
-//   {
-//     id: 3,
-//     name: {
-//       alias: 'mike',
-//       lastname: 'mike',
-//       surname: 'tyson',
-//     },
-//     age: 52,
-//   },
-// ];
+//decontructing complex arrays
+const people = [
+  {
+    id: 1,
+    name: {
+      alias: 'kunle',
+      lastname: 'adekunle',
+      surname: 'ashaolu',
+    },
+    age: 20,
+  },
+  {
+    id: 2,
+    name: {
+      alias: 'jay',
+      lastname: 'jayson',
+      surname: 'tatum',
+    },
+    age: 26,
+  },
+  {
+    id: 3,
+    name: {
+      alias: 'mike',
+      lastname: 'mike',
+      surname: 'tyson',
+    },
+    age: 52,
+  },
+];
 
-// function whoIs({ name: { alias, lastname, surname }, age }) {
-//   return console.log(`meet ${lastname} ${surname} , he's ${age} years old, also known as ${alias}`);
-// }
+function whoIs({ name: { alias, lastname, surname }, age }) {
+  return console.log(`meet ${lastname} ${surname} , he's ${age} years old, also known as ${alias}`);
+}
 
-// whoIs(people[0]);
+whoIs(people[0]);
 
-// // swapping variables
-// let a = 1;
-// let b = 2;
+// swapping variables
+let a = 1;
+let b = 2;
 
-// // exchange the values of a & b
-// [b, a] = [a, b];
-// console.log('a = ' + a, ', b = ' + b);
+// exchange the values of a & b
+[b, a] = [a, b];
+console.log('a = ' + a, ', b = ' + b);
 
-// exchange array values
-// const swapArray = [10, 40];
-// [swapArray[0], swapArray[1]] = [swapArray[1], swapArray[0]];
-// console.log(swapArray);
+//exchange array values
+const swapArray = [10, 40];
+[swapArray[0], swapArray[1]] = [swapArray[1], swapArray[0]];
+console.log(swapArray);
