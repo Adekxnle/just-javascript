cfceb41de1c9e22bdcf9fa83d60e2fa1200740ca
Adekunle Ashaolu <dekxnle@gmail.com>
2022-05-02 03:40:57 +0100

    new examples in new file

--- destructuring.js
+++ destructuring.js
@@ -1,9 +1,39 @@
-// // decontructing complex arrays
-// const users = [
-//   { id: 1, name: 'kunle', age: 20 },
-//   { id: 2, name: 'jackson', age: 16 },
-//   { id: 3, name: 'olamide', age: 32 },
+// decontructing complex arrays
+// const people = [
+//   {
+//     id: 1,
+//     name: {
+//       alias: 'kunle',
+//       lastname: 'adekunle',
+//       surname: 'ashaolu',
+//     },
+//     age: 20,
+//   },
+//   {
+//     id: 2,
+//     name: {
+//       alias: 'jay',
+//       lastname: 'jayson',
+//       surname: 'tatum',
+//     },
+//     age: 26,
+//   },
+//   {
+//     id: 3,
+//     name: {
+//       alias: 'mike',
+//       lastname: 'mike',
+//       surname: 'tyson',
+//     },
+//     age: 52,
+//   },
 // ];
+
+// function whoIs({ name: { alias, lastname, surname }, age }) {
+//   return console.log(`meet ${lastname} ${surname} , he's ${age} years old, also known as ${alias}`);
+// }
+
+// whoIs(people[0]);
 
 // // swapping variables
 // let a = 1;
